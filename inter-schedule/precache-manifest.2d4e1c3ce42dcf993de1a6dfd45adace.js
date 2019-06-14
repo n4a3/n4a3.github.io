f973b9ae8e7b9855ac421d09eb6b9cb4121dbd49
n4a3 <>
2019-06-14 14:36:01 +0400

    add project "Intetractive schedule"

---
+++ inter-schedule/precache-manifest.2d4e1c3ce42dcf993de1a6dfd45adace.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd4185e2a3949002ff04",
+    "url": "./static/css/main.ebce7053.chunk.css"
+  },
+  {
+    "revision": "bd4185e2a3949002ff04",
+    "url": "./static/js/main.bd4185e2.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "e0ca91786f58f7267957",
+    "url": "./static/js/2.e0ca9178.chunk.js"
+  },
+  {
+    "revision": "3dfc87ec0c9c3da64d8255f341b693ae",
+    "url": "./index.html"
+  }
+];
