f068ae14666edbcebd0ea377cbde90778d0ff254
n4a3 <>
2019-06-21 11:49:29 +0400

    add test for mercdev

---
+++ mercdev-test/precache-manifest.bece68cf2f1cb3ae52fb1e81aad3461e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdbde0adf5ad3caec0921cc603b47cb7",
+    "url": "/mercdev-test/index.html"
+  },
+  {
+    "revision": "9e18592f2590fba7e32d",
+    "url": "/mercdev-test/static/css/main.e2769ed8.chunk.css"
+  },
+  {
+    "revision": "add7e156698c6d7162b4",
+    "url": "/mercdev-test/static/js/2.2b5943a6.chunk.js"
+  },
+  {
+    "revision": "9e18592f2590fba7e32d",
+    "url": "/mercdev-test/static/js/main.8d89d20f.chunk.js"
+  },
+  {
+    "revision": "3d4f143fde8a2245daae",
+    "url": "/mercdev-test/static/js/runtime~main.4f559749.js"
+  },
+  {
+    "revision": "39b1c119caf33ce82b61e6b5f6a6c53e",
+    "url": "/mercdev-test/static/media/GothamBold.39b1c119.woff"
+  },
+  {
+    "revision": "db33e70bc9dee9fa9ae9737ad83d77ba",
+    "url": "/mercdev-test/static/media/GothamBold.db33e70b.ttf"
+  },
+  {
+    "revision": "77171d8f5b5283f9d47a3434704bf944",
+    "url": "/mercdev-test/static/media/GothamMedium.77171d8f.ttf"
+  },
+  {
+    "revision": "ba7ee4ffcdcd65f753a35cd804ac1514",
+    "url": "/mercdev-test/static/media/GothamMedium.ba7ee4ff.woff"
+  },
+  {
+    "revision": "b6065e968c887b68796e4b78dc8a0609",
+    "url": "/mercdev-test/static/media/background.b6065e96.jpg"
+  },
+  {
+    "revision": "c96355a9f8032d7ae18b4776ad8c1fb5",
+    "url": "/mercdev-test/static/media/background@2x.c96355a9.jpg"
+  },
+  {
+    "revision": "15aa058167caac2c8bf4275336646824",
+    "url": "/mercdev-test/static/media/w-mercury-development.15aa0581.svg"
+  }
+]);
